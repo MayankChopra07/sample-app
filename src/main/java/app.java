d3e730482e223893eeca633d8f692c676804a49e
MayankChopra07 <mayankchopra22222@gmail.com>
2025-12-14 22:13:06 +0530

    Initial commit

---
+++ src/main/java/app.java
@@ -0,0 +1,6 @@
+public class app {
+    public static void main(String[] args) {
+        System.out.println("Hello from Automated Jenkins Build!");
+        System.out.println("Triggered by GitHub push!");
+    }
+}
